407981b132af2da23e77f49a92c1809bfe553c5a
nowa-speisekammer <emil.kraft@fau.de>
2026-08-26 09:31:20 +0200

    v39

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // Automatisch generiert von generate_app.py - nicht manuell bearbeiten.
 // Bei jedem Skriptlauf aendert sich CACHE_VERSION, wodurch alte Caches
 // beim naechsten Seitenaufruf automatisch ersetzt werden.
-const CACHE_VERSION = "20260826-092256";
+const CACHE_VERSION = "20260826-093058";
 const CACHE_NAME = "nowa-cache-" + CACHE_VERSION;
 
 // Alles, was fuer die Offline-Nutzung vorab gecacht werden soll.
